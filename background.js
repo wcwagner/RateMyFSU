2c8c181f0ad98651f71bd34d0f3870114c6c194c
William Wagner <wcw13@my.fsu.edu>
2016-04-13 06:55:13 -0400

    update

--- background.js
+++ background.js
@@ -10,9 +10,42 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 	chrome.pageAction.show(tabId);
 	}
 	// ... show the page action.
-
-
+}
+var queryBaseUrl = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName"+
+								"&schoolName=florida%20state%20university&queryoption=HEADER"+
+								"&query=";
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse){
+		if(request.profNames.length > 0){
+			request.profNames.forEach(function(prof){
+				var queryUrl = queryBaseUrl + prof + "&facetSearch=true";
+				console.log(queryUrl);
+			});
+		}
+		sendResponse({farewell: "goodbye"});
+	});
+var url = "http://www.ratemyprofessors.com/search.jsp?queryBy=teacherName&schoolName=florida%20state%20university&queryoption=HEADER&query=xiuwen%20liu&facetSearch=true";
+var xhr = new XMLHttpRequest();
+xhr.open("GET", url, true);
+xhr.send();
+xhr.onreadystatechange = function(){
+	if( xhr.readyState == 4 && xhr.status == 200){
+		console.log("Success");
+		// Turn XMLHttpresponse into jQuery dom object
+		var $responseDOM = $($.parseHTML(xhr.response));
+		var teacherHref = $responseDOM.find('li[class="listing PROFESSOR"] a').attr('href');
+		//grab teacher id from href
+		var profPageUrl = "http://ratemyprofessors.com" + teacherHref;
+		console.log(profPageUrl);
+		/*var ratingXHR = new XMLHttpRequest();
+		ratingXHR.open(GET)
+		ratingXHR.send();
+		ratingXHR.onreadystatechange = function(){
+			if( ratingXHR.readyState == 4 && ratingXHR.status == 200){
+				$ratingDOM = $($.parseHTML(ratingXHR.response));
+				var rating = $ratingDOM.find('div[class="grade"]').text();
+			}
+		};*/
+	}
 };
 
-// Listen for any changes to the URL of any tab.
-chrome.tabs.onUpdated.addListener(checkForValidUrl);
--- main.js
+++ main.js
@@ -4,28 +4,60 @@ function delayedListener(){
 	timeOut = window.setTimeout(checkForSearchResultsPage, 500);
 	//console.log("Started delayedListener " + timeOut);
 }
-
 function cancelListener(){
 	//console.log("Cancelling delayedListener " + timeOut);
 	window.clearTimeout(timeOut);
 
 }
-
 function checkForSearchResultsPage(){
-	var checkForProfessor;
+	if(document.getElementById("MTG_INSTR$0"))
+		grabProfessors();
+	/*var checkForProfessor;
 	 try{
 	 	checkForProfessor = document.getElementById("MTG_INSTR$0").innerHTML;
 	 }
 	 catch(professorNotFound){}
 	 if(checkForProfessor !== undefined){
-	 	console.log("PAGE");
 	 		grabProfessors();
-	 }
+	 }*/
 }
 
 function grabProfessors(){
+	var regex = /[A-z]+\ [A-z]+/;
+	var count = 0;
+	var currProfId = "MTG_INSTR$" + count;
+	var currProfName = "";
+	var Professors = [];
+	var ProfObj = {};
+	do{
+
+		currProfId = "MTG_INSTR$" + count;
+		currProfElem = document.getElementById(currProfId);
+		if( currProfElem){
+			currProfName = currProfElem.innerHTML;
+			currProfName = currProfName.match(regex);
+			if(!ProfObj[currProfName]){
+				console.log("Adding " + currProfName + " to object");
+				ProfObj[currProfName] = [];
+				ProfObj[currProfName][0] = "";
+			}
+			ProfObj[currProfName].push(currProfId);
+			Professors.push(currProfName);
+		}
+		count++;
+	}while(currProfElem !== null);
+	console.log(ProfObj);
+	for(var key in ProfObj)
+	getProfessorRatings(ProfObj);
+}
+
+function getProfessorRatings(profArray){
+	chrome.runtime.sendMessage(ProfObj, function(response){
+		//console.log(response.farewell);
+	});
 
 }
+
 var observer = new MutationObserver(function(mutations){
 	mutations.forEach(function(mutation){
 		//The target that is affected when the class search page is reached
